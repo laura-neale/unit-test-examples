6b22eb6f4b39264447e4ccb559e53c97441e3665
laura-neale <lauran@brandwatch.com>
2017-03-17 11:52:16 +0000

    added simple mock test using spock framework

--- src/main/java/PiggyFarm.java
+++ src/main/java/PiggyFarm.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class PiggyFarm {
+
+    private List<Piggy> piggies;
+
+    public PiggyFarm(List<Piggy> piggies) {
+        this.piggies = piggies;
+    }
+
+    public Piggy getSmallestPiggy() {
+        return piggies.stream()
+                .reduce((a,b) -> getSmallerPiggy(a, b))
+                .orElseThrow(() -> new NoSuchElementException("No piggies found"));
+    }
+
+    private Piggy getSmallerPiggy(Piggy a, Piggy b) {
+        if (a.getWeight() < b.getWeight()) {
+            return a;
+        } else {
+            return b;
+        }
+    }
 }
--- src/test/java/PiggyFarmTest.groovy
+++ src/test/java/PiggyFarmTest.groovy
@@ -1,5 +1,26 @@
 import spock.lang.Specification;
 
-public class PiggyFarmTest extends Specification {
+/*
+Simple test using spock mocking framework
+ */
+class PiggyFarmTest extends Specification {
+
+    def pigA = Mock(Piggy)
+    def pigB = Mock(Piggy)
+    def pigC = Mock(Piggy)
+
+    def "get smallest piggy returns smallest piggy in farm"() {
+        given: ""
+        pigA.weight >> 0
+        pigB.weight >> 1
+        pigC.weight >> 2
+        def farm = new PiggyFarm([pigA, pigB, pigC])
+
+        when: ""
+        def smallestPiggy = farm.smallestPiggy
+
+        then: ""
+        smallestPiggy.is(pigA)
+    }
 
 }
